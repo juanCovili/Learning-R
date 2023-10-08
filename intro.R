0e00f094b4f6fb6690e39571686f3770fe5b5e8c
Juan Covili <juancovrex@gmail.com>
2023-10-08 01:08:54 -0300

    utilice los metodos typeof, print, paste y cree variables

---
+++ intro.R
@@ -0,0 +1,23 @@
+print('hello world')
+#nombres = 'Juan Covili'
+temperatura <- 50.5
+licenciaMedica <- T
+#edad=8
+
+#typeof(edad)
+#typeof(nombres)
+typeof(temperatura)
+typeof(licenciaMedica)
+
+edad <- as.integer(edad)
+typeof(edad)
+
+#as.double
+
+nombres <- readline(prompt = "Ingresa tu nombre : ")
+edad <- as.integer(readline(prompt = "Ingresa tu edad : "))
+
+print(
+  paste("Tu nombre : ",nombres, " tu edad : ", edad, " años")
+)
+
