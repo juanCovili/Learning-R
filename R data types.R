a7cda1ecd207edf2b3c69683b72439f9146ca34d
Juan Covili <juancovrex@gmail.com>
2023-10-09 02:30:34 -0300

    Ejemplo tipo de datos sin prefijo L

---
+++ R data types.R
@@ -0,0 +1,19 @@
+# https://www.w3schools.com/r/r_data_types.asp
+
+# Tipo de dato : Valor
+# Numeric (Double) : 36.5
+# Integer (int) (para indicar que es tipo de dato entero, se debe añadir prefijo L) : 32
+# Character (String) : Juan
+# Complex (Números complejos) 3 + 2i
+# Logical (Boolean) : TRUE/FALSE | 1,0 | T,F
+
+# Ejemplo tipo de datos (Sin prefijo L)
+edad <- 32
+temperatura <- 36.5
+nombres <- "Juan"
+licenciaMedica <- F
+
+typeof(edad)
+typeof(temperatura)
+typeof(nombres)
+typeof(licenciaMedica)
